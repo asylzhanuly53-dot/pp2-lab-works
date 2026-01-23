9a07935e8229f5f6ffeb406064b4f2a69f0392f9
Nurasyl <asylzhanuly53@gmail.com>
2026-01-23 19:22:54 +0500

    added all examples with practice 1

---
+++ Desktop/PP2/practice_1/Syntax/statements.py
@@ -0,0 +1,13 @@
+print("Python is fun!")
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+#That's all right.
+
+
+#SyntaxError: invalid syntax
+#print("Python is fun!") print("Really!")
