0a15abf0f885b47044b4d94414f78a370c060f1e
Nurasyl <asylzhanuly53@gmail.com>
2026-01-31 01:54:13 +0500

    added examples from the While loop topic

---
+++ Desktop/PP2/practice_2/while_loop/while_loop.py
@@ -0,0 +1,13 @@
+# With the while loop we can execute a set of statements as long as a condition is true.
+
+# Example
+# Print i as long as i is less than 6:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+#   Note: remember to increment i, or else the loop will continue forever.
+
+# The while loop requires relevant variables to be ready, 
+# in this example we need to define an indexing variable, i, which we set to 1.
