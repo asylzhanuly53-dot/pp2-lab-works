732d6271ad55f7e67cc4996c412fdf240b5fbd33
Nurasyl <asylzhanuly53@gmail.com>
2026-01-31 01:52:10 +0500

    added examples from the Booleans topic

---
+++ Desktop/PP2/practice_2/Booleans/evalueate.py
@@ -0,0 +1,7 @@
+# he bool() function allows you to evaluate any value, and give you True or False in return,
+
+# Example
+# Evaluate a string and a number:
+
+print(bool("Hello"))
+print(bool(15))
---
+++ Desktop/PP2/practice_2/Booleans/functions.py
@@ -0,0 +1,21 @@
+# You can create functions that returns a Boolean Value:
+
+# Example
+# Print the answer of a function:
+
+def myFunction() :
+  return True
+
+print(myFunction())
+
+
+
+# Print "YES!" if the function returns True, otherwise print "NO!":
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
