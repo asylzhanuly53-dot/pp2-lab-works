114b9b822e3128c38980995e6a0a21a5b60894ed
Nurasyl <asylzhanuly53@gmail.com>
2026-04-20 22:59:55 +0500

    Add Practice9 - Pygame games: Mickey's clock, music player, moving ball

---
+++ practice_9/music_player/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+COLOR_RED       = (220, 60,  60)
+COLOR_YELLOW    = (255, 220, 0)
+
+
+def draw_progress_bar(screen, x, y, width, height, position_sec, color_fill, color_bg):
+    """Draw a simple animated progress bar (position c
